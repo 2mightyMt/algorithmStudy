4a0fa44cb62a9ec4a26f66ac3c09ce72d454db43
JwLee6520 <dlwjddn6520@gmail.com>
2026-03-31 10:10:03 +0900

    [boj] 별찍기 1

--- JwLee6520/baekjoon/3_반복문/9_2438.c
+++ JwLee6520/baekjoon/3_반복문/9_2438.c
@@ -3,7 +3,7 @@ int main() {
 	int a; //별 개수 정하는 변수 하나 선언
 	scanf("%d", &a); // scanf로 입력받은 값을 a에 저장
 
-	for (int i = 1; i <= a; i++) {
+	for (int i = 1; i <= a; i++) { 
 		for (int j = 0; j < i; j++) {
 			printf("*");
 		}
