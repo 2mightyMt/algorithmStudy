e546cb09c3ae39eee52940b62695351ee34ea4ca
JwLee6520 <dlwjddn6520@gmail.com>
2026-03-31 09:10:19 +0900

    [boj] A + B - 7

--- JwLee6520/baekjoon/3_반복문/7_11021.c
+++ JwLee6520/baekjoon/3_반복문/7_11021.c
@@ -1,1 +1,12 @@
-........
+#include <stdio.h>
+int main() {
+	int in, a, b; //입력받을값 in, 더할 값 a, b 선언
+	scanf("%d", &in); //scanf로 in의 값 할당
+
+	for (int i = 1; i <= in; i++) { // 입력받은 in의 수 만큼 반복문 실행
+		scanf("%d %d", &a, &b); // 더할 값 ab 입력
+		int result = b + a; // a와 b를 더한 값을 result에 할당
+		printf("Case #%d : %d\n", i, result); //결과값 출력
+	}
+	return 0;
+}
